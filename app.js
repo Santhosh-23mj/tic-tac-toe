9d76ef914ffc1fa17ef2f5d555cc6f0b20e22e2f
n00bie <santhosh23mj@gmail.com>
2021-08-08 17:25:34 +0530

    remove unnecessary code, packages and files

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const http = require('http');
+const socketServer = require('socket.io').Server;
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const httpServer = http.createServer();
+const io = new socketServer(httpServer);
+const port = normalizePort(process.env.PORT || '3000');
 
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+httpServer.listen(port, () => {
+    console.log(`[INFO] Listening on port - ${port}`)
 });
+httpServer.on('error', onError);
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+module.exports.io = io;
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+//
+// Normalize port number to an Integer
+//
+function normalizePort(val) {
+    const port = parseInt(val, 10);
 
-module.exports = app;
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+//
+// Handle errors while binding to a PORT
+//
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
